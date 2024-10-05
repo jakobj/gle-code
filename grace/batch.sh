5bb7e7a23f3e2010091493b4ca0d512780cc4661
Jakob Jordan <jakobjordan@posteo.de>
2024-10-05 14:04:45 -0400

    Add batch script for grace

---
+++ grace/batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=gle-fp16
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=pi_manohar
+#SBATCH --time=12:00:00
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.out
+module purge
+module load Python/3.10.8-GCCcore-12.2.0
+cd /home/jj845/project/gle-code/
+source venv/bin/activate
+date
+OMP_NUM_THREADS=12 time python -m experiments.mnist1d.plastic_e2e --seed=42 --epochs 80 --precision single --lr 5e-3 --optimizer-step-interval 1 --tau_r-scaling 1.0 --scheduler step
+date
