2e8cb2409c1b30a069f8e16cf879262599c4891c
Jakob Jordan <jakobjordan@posteo.de>
2024-10-13 20:00:58 -0400

    Update batch script

--- grace/batch.sh
+++ grace/batch.sh
@@ -11,5 +11,5 @@ module load Python/3.10.8-GCCcore-12.2.0
 cd /home/jj845/project/gle-code/
 source venv/bin/activate
 date
-OMP_NUM_THREADS=12 time python -m experiments.mnist1d.plastic_e2e --seed=42 --epochs 80 --precision half --lr 5e-3 --optimizer-step-interval 1 --tau_r-scaling 1.0 --scheduler step
+OMP_NUM_THREADS=12 time python -m experiments.mnist1d.plastic_e2e --seed=12 --epochs 8 --precision-parameters single --precision-dynamics half --lr 5e-3 --optimizer-step-interval 1 --tau_r-scaling 1.0 --scheduler step
 date
